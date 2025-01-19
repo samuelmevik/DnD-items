3eb469c2385f01b7edfb79d2c3ef0176732e48de
samuelmevik <samuel.mevik@outlook.com>
2025-01-19 20:14:56 +0100

    visual update

--- src/components/FilterSidebar.tsx
+++ src/components/FilterSidebar.tsx
@@ -2,6 +2,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { Slider } from "@/components/ui/slider";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import { useState } from "react";
 
 type FilterSidebarProps = {
   allTags: string[];
@@ -12,20 +13,29 @@ type FilterSidebarProps = {
   maxPrice: number;
   onMaxPriceChange: (price: number) => void;
   availablePriceRange: [number, number];
-}
+};
 
-function FilterSection({allTags, selectedTags, onTagChange} : { allTags: string[], selectedTags: string[], onTagChange: (tags: string[]) => void }) {
+function FilterSection({
+  allTags,
+  selectedTags,
+  onTagChange,
+}: {
+  allTags: string[];
+  selectedTags: string[];
+  onTagChange: (tags: string[]) => void;
+}) {
   const handleTagChange = (tag: string) => {
     const newTags = selectedTags.includes(tag)
       ? selectedTags.filter((t) => t !== tag)
-      : [...selectedTags, tag]
-    onTagChange(newTags)
-  }
+      : [...selectedTags, tag];
+    onTagChange(newTags);
+  };
   return (
     <>
-      <h2 className="text-2xl font-semibold mb-4">Filters</h2>
       <div className="md:mb-6">
-        <h3 className="text-lg font-medium mb-2 overflow-auto hidden md:block">Tags</h3>
+        <h3 className="text-lg font-medium mb-2 overflow-auto hidden md:block">
+          Tags
+        </h3>
         <ScrollArea>
           <div className="grid grid-flow-col grid-rows-2 md:grid-flow-row gap-x-8">
             {allTags.map((tag) => (
@@ -45,7 +55,7 @@ function FilterSection({allTags, selectedTags, onTagChange} : { allTags: string[
         </ScrollArea>
       </div>
     </>
-  )
+  );
 }
 
 function PricingSection({
@@ -54,7 +64,7 @@ function PricingSection({
   maxPrice,
   onMaxPriceChange,
   availablePriceRange,
-} : {
+}: {
   minPrice: number;
   onMinPriceChange: (price: number) => void;
   maxPrice: number;
@@ -63,33 +73,33 @@ function PricingSection({
 }) {
   return (
     <div>
-    <h3 className="text-lg font-medium mb-2">Price Range</h3>
-    <div>
-      <p>Minimum Price</p>
-      <Slider
-        min={availablePriceRange[0]}
-        max={availablePriceRange[1]}
-        step={1}
-        value={[minPrice]}
-        onValueChange={([value]) => onMinPriceChange(value)}
-        className="mb-2"
-      />
-      <span>{minPrice} gp</span>
+      <h3 className="text-lg font-medium mb-2">Price Range</h3>
+      <div>
+        <p>Minimum Price</p>
+        <Slider
+          min={availablePriceRange[0]}
+          max={availablePriceRange[1]}
+          step={1}
+          value={[minPrice]}
+          onValueChange={([value]) => onMinPriceChange(value)}
+          className="mb-2"
+        />
+        <span>{minPrice} gp</span>
+      </div>
+      <div>
+        <p>Maximum Price</p>
+        <Slider
+          min={availablePriceRange[0]}
+          max={availablePriceRange[1]}
+          step={1}
+          value={[maxPrice]}
+          onValueChange={([value]) => onMaxPriceChange(value)}
+          className="mb-2"
+        />
+        <span>{maxPrice} gp</span>
+      </div>
     </div>
-    <div>
-      <p>Maximum Price</p>
-      <Slider
-        min={availablePriceRange[0]}
-        max={availablePriceRange[1]}
-        step={1}
-        value={[maxPrice]}
-        onValueChange={([value]) => onMaxPriceChange(value)}
-        className="mb-2"
-      />
-      <span>{maxPrice} gp</span>
-    </div>
-  </div>
-  )
+  );
 }
 
 export default function FilterSidebar({
@@ -102,16 +112,29 @@ export default function FilterSidebar({
   onMaxPriceChange,
   availablePriceRange,
 }: FilterSidebarProps) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="min-w-64">
-      <FilterSection allTags={allTags} selectedTags={selectedTags} onTagChange={onTagChange} />
-      <PricingSection
-        minPrice={minPrice}
-        onMinPriceChange={onMinPriceChange}
-        maxPrice={maxPrice}
-        onMaxPriceChange={onMaxPriceChange}
-        availablePriceRange={availablePriceRange}
-      />
+    <div className="min-w-64 bg-white h-fit p-4 rounded-xl shadow-md relative">
+      <h2 className="text-2xl font-semibold mb-4">Filters</h2>
+      <button onClick={() => setIsOpen(!isOpen)} className="absolute top-0 right-0 p-4">
+        {isOpen ? "Hide" : "Show"}
+      </button>
+      {isOpen && (
+        <>
+          <FilterSection
+            allTags={allTags}
+            selectedTags={selectedTags}
+            onTagChange={onTagChange}
+          />
+          <PricingSection
+            minPrice={minPrice}
+            onMinPriceChange={onMinPriceChange}
+            maxPrice={maxPrice}
+            onMaxPriceChange={onMaxPriceChange}
+            availablePriceRange={availablePriceRange}
+          />
+        </>
+      )}
     </div>
   );
 }
